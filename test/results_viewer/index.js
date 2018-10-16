27884786615dbb9a37a3c6bcb8815a4eb23010fa
iduarte <iduarte@skylinenet.net>
2018-10-16 16:16:17 -0400

    WIP

--- test/results_viewer/index.js
+++ test/results_viewer/index.js
@@ -59,14 +59,59 @@ async function generateResults (directory) {
 
 const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const csvDirectory = path.join(__dirname, '..', 'soak');
 
-  const csvDirectory = path.join(__dirname, '..', 'soak');
+    const results = await generateResults(csvDirectory);
 
-  const results = await generateResults(csvDirectory);
+    const send = `
+                                    <!DOCTYPE html>
+                     <html>
 
-  res.end(JSON.stringify(results));
+<header>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css" rel="stylesheet" />
+
+</header>
+
+<body>
+<div id="chart" class="c3"></div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js"></script>
+<script>
+var chart = c3.generate({
+    data: {
+        x: 'x',
+        columns: [
+            ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 130, 340, 200, 500, 250, 350]
+        ]
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%Y-%m-%d'
+            }
+        }
+    }
+});
+
+setTimeout(function () {
+    chart.load({
+        columns: [
+            ['data3', 400, 500, 450, 700, 600, 500]
+        ]
+    });
+}, 1000);
+</script>
+
+</body>
+</html>
+`
+   res.end(send);
+  }
 });
 
 server.listen(port, hostname, () => {
