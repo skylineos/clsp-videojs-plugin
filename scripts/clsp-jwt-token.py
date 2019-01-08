f3e92d660cebb1c631de214d9eed46d79007a3a8
David Schere <dschere@skylinenet.net>
2019-01-08 15:12:59 -0500

    Test script that create clsp-jwt urls

---
+++ scripts/clsp-jwt-token.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+import time
+import base64
+
+
+streamName = sys.argv[1]
+sfsAddr = sys.argv[2]
+
+expire = 15
+
+start = int(time.time())
+end = start + (expire * 60)
+access_url = "clsp-jwt://"+sfsAddr+"/"+"?Start="+str(start)+"&End="+str(end)
+
+url_create = "http://127.0.0.1:3100/create"
+
+payload = {
+    "B64url" : base64.b64encode(streamName),
+    "B64accessUrl": base64.b64encode(access_url),
+    "Expires": expire
+}
+
+res = requests.post(url_create, json=payload)
+
+jwt = res.content
+
+print "clsp-jwt://"+sfsAddr+"/"+jwt+"?Start="+str(start)+"&End="+str(end)
+
+
+
